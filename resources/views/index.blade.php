f2eb4bb91eb1cada65511e893d84966405a003a2
Elton Fonseca <elton869@gmail.com>
2021-04-17 17:04:04 -0300

    13 - Loops While e For

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -120,9 +120,9 @@
                     </div>
                     <nav aria-label="Page navigation example" style="margin-top: 50px;">
                         <ul class="pagination pagination-lg">
-                          <li class="page-item"><a class="page-link" href="#">1</a></li>
-                          <li class="page-item"><a class="page-link" href="#">2</a></li>
-                          <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            @for($i=1; $i < 10; $i++)
+                                <li class="page-item"><a class="page-link" href="#">{{ $i }}</a></li>
+                            @endfor
                         </ul>
                     </nav>
                 </div>
