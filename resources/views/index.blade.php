7d19eed11ff5a0c5f58a75567afee109da647354
Elton Fonseca <elton869@gmail.com>
2021-04-21 07:59:42 -0300

    16 - Variável loop

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -68,7 +68,7 @@
                         @continue($projeto['ativo'] === false)
 
                         <div class="col-md-6 col-lg-4 mb-5">
-                            <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                            <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal{{ $loop->iteration }}">
                                 <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                     <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                                 </div>
