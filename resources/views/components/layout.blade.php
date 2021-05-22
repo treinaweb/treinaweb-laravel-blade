efc322682b34e4ef9a52f36e9a8700645a6cc242
Elton Fonseca <elton869@gmail.com>
2021-05-22 11:41:54 -0300

    33 - Criando View Sobre e Contato com componente Layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,7 +20,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                <a class="navbar-brand js-scroll-trigger" href="{{ route('site.componente.home') }}">Start Bootstrap</a>
                 <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -30,15 +30,15 @@
                         $itensMenu = [
                             [
                                 'descricao' => 'Portfolio',
-                                'link' => '#portfolio'
+                                'link' => route('site.componente.portfolio')
                             ],
                             [
                                 'descricao' => 'Sobre',
-                                'link' => '#about'
+                                'link' => route('site.componente.sobre')
                             ],
                             [
                                 'descricao' => 'Contato',
-                                'link' => '#contact'
+                                'link' => route('site.componente.contato')
                             ]
                         ];
                     @endphp
