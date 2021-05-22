e2777cbca814e22be9386f29e401be58cc26f018
Elton Fonseca <elton869@gmail.com>
2021-05-22 10:11:33 -0300

    29 - Criando o primeiro componente

--- resources/views/componente/portfolio.blade.php
+++ resources/views/componente/portfolio.blade.php
@@ -66,14 +66,7 @@
                     @forelse ($projetos as $projeto)
                         @continue($projeto['ativo'] === false)
 
-                        <div class="col-md-6 col-lg-4 mb-5">
-                            <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal{{ $loop->iteration }}">
-                                <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
-                                </div>
-                                <img class="img-fluid" src="/assets/img/portfolio/{{ $projeto['imagem'] }}" alt="..." />
-                            </div>
-                        </div>
+                        <x-item-projeto :imagem="$projeto['imagem']" :indice="$loop->iteration" />  
                     @empty
                         <h1>Nenhum projeto encontrado</h1>    
                     @endforelse
